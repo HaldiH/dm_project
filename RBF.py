8ae8f4f9856a76fef9e709edfe2301b15fb57353
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2022-06-19 17:25:14 +0200

    WIP RBF coviariance matrix

--- RBF.py
+++ RBF.py
@@ -1,32 +1,37 @@
 from __future__ import annotations
-from typing import Optional
+from typing import List, Optional
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
 
 class RBF(object):
-    def __init__(self, n_clusters=8, sigma=1.0):
+    def __init__(self, n_clusters=8):
         self.n_clusters = n_clusters
-        self.sigmas = sigma * np.ones((n_clusters,))
+        self.sigmas: Optional[List[np.ndarray]] = None
         self.kmeans: Optional[KMeans] = None
         self.lr: Optional[LinearRegression] = None
 
-    def __phi(x, mu, sigma):
-        return np.exp(- np.linalg.norm(x - mu, axis=1) ** 2 / (2 * sigma**2))
+    def phi(x, mu, sigma: np.ndarray):
+        print(np.shape(x-mu))
+        print(np.shape(sigma))
+        return np.exp(-1/2 * (x - mu).T @ np.linalg.inv(sigma) @ (x - mu))
 
-    def get_phi(self, x):
-        return np.array([RBF.__phi(x, center, sigma)
-                         for center, sigma in zip(self.kmeans.cluster_centers_, self.sigmas)]).T
+    def __phi(self, x):
+        return np.array([RBF.phi(x, center, sigma) for center, sigma in zip(self.kmeans.cluster_centers_, self.sigmas)])
 
     def fit(self, X, y) -> RBF:
         self.kmeans: KMeans = KMeans(n_clusters=self.n_clusters).fit(X)
+        pred = self.kmeans.predict(X)
+        print(X[pred == 0].shape)
+        self.sigmas = [np.cov(X[pred == cluster])
+                       for cluster in range(self.n_clusters)]
         # phi_j is a scalar representing the activation of the jth cluster wrt its distance with x
         # so for each cluster we need to estimate the best weights
-        phi = self.get_phi(X)
+        phi = self.__phi(X)
         self.lr = LinearRegression().fit(phi, y)
         return self
 
     def predict(self, X):
-        phi = self.get_phi(X)
+        phi = self.phi(X)
         return self.lr.predict(phi)
